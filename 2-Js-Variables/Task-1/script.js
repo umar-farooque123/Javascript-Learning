0b7dc55a91f8e681c5c1f05d39bd8998ebd913a4
umar-farooque123 <umarfarooqueshaikh344@gmail.com>
2026-01-29 19:21:45 +0500

    2nd folder

---
+++ 2-Js-Variables/Task-1/script.js
@@ -0,0 +1,8 @@
+
+// let name = "Umar Farooque";
+// const age = 20;
+// let city = "Hyderabad";
+
+// name = "Usman";
+
+// console.log(name + age + city);
---
+++ 2-Js-Variables/Task-2/script.js
@@ -0,0 +1,26 @@
+
+
+// Ask the user for their name
+// let name = prompt("Enter your name:");
+
+// Ask the user for their age
+// let age = +prompt("Enter your age:");
+
+// Create the greeting message using + for concatenation
+// let message = "Hello, " + name + "! You are " + age + " years old.";
+
+// // Display message in popup
+// alert(message);
+
+
+// Display mesage in console
+
+// let name = "Umar";  // Replace with any name
+// let age = 26;        // Replace with any age
+
+// Create the greeting message
+// let message = "Hello, " + name + "! You are " + age + " years old.";
+
+// Display message in console
+// console.log(message);
+
